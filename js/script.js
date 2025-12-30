2b7bce7d70ea5ef9844e74bee9ddaeffa3f1f288
ygomes10 <yuritecnologiadainformacao@gmail.com>
2025-12-30 13:45:50 -0300

    meu primeiro commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let nomeHeroi = "Rock"
+let xpHeroi = 10
+let nivelHeroi = "";
+
+if (xpHeroi < 1000) {
+    nivelHeroi = "Ferro";
+} else if (xpHeroi <= 2000) {
+    nivelHeroi = "Bronze";
+} else if (xpHeroi <= 5000) {
+    nivelHeroi = "Prata";
+} else if (xpHeroi <= 7000) {
+    nivelHeroi = "Ouro";
+} else if (xpHeroi <= 8000) {
+    nivelHeroi = "Platina";
+} else if (xpHeroi <= 9000) {
+    nivelHeroi = "Ascendente";
+} else if (xpHeroi <= 10000) {
+    nivelHeroi = "Imortal";
+} else {
+    nivelHeroi = "Radiante";
+}
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível " + nivelHeroi );
+
